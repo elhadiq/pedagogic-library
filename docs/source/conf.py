f92ca59a8b919669b3aa939ab269e1f10992b353
ZOUHAIR EL HADIQ <elhadiq.pro@gmail.com>
2023-12-07 19:14:45 +0100

    .readthedocs yaml update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'cpgetools'
+copyright = '2023, elhadiq'
+author = 'elhadiq'
 
 release = '0.1'
 version = '0.1.0'
